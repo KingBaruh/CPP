72217e725b0aa3df6516f3e4ad7154d4f88ea94e
Baruh Riss <167196715+KingBaruh@users.noreply.github.com>
2024-11-15 18:54:26 +0200

    Update Tablet.cpp

--- Assignment4/Tablet.cpp
+++ Assignment4/Tablet.cpp
@@ -1,6 +1,3 @@
-// Student Baruh Riss, 318739174
-// Student Baruh Krugliak, 211641956
-
 #include "Tablet.h"
 
 // Constructor that initializes a Tablet with its price, manufacturer, color, CPU, number of ports, and screen size
